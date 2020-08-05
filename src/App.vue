2895570ab40b381ee66be6184943497ad01d4027
otavio <otavio@leanonsystems.com>
2020-08-05 09:38:31 -0300

    Created the page's layout. It connects to the questions api and gets random questions everytime

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="8" offset="2">
+          <questionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/header.vue'
+import questionBox from './components/questionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    questionBox
+  },
+  data(){
+    return{
+      questions: [],
+      index: 0
+    }
+  },
+  methods:{
+    next(){
+      this.index++
+    }
+  },
+  mounted: function(){
+    fetch("https://opentdb.com/api.php?amount=10&category=15&type=multiple", {
+      method: "get"
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
+      })
   }
 }
 </script>
---
+++ src/components/questionBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron class="mt-5">
+            <template v-slot:lead>
+                {{ currentQuestion.question }}
+            </template>
+
+            <hr class="my-4">
+
+            <b-list-group class="mb-4">
+                <b-list-group-item v-for="(answer, index) in answers" :key="index">
+                    {{ answer }}
+                </b-list-group-item>
+            </b-list-group>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button @click="next" variant="success" href="#">Next Question</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            currentQuestion: Object,
+            next: Function
+        },
+        computed:{
+            answers(){
+                let answers = [...this.currentQuestion.incorrect_answers]
+                answers.push(this.currentQuestion.correct_answer)
+                return answers
+            }
+        }
+    }
+</script>
